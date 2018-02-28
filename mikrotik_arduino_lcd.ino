6f28e542b63e68e94242a8fd002839834a84bf9e
Eugene Samoilov <eugene.samoilov@gmail.com>
2018-02-28 19:24:40 +0900

    first

---
+++ mikrotik_arduino_lcd.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    delay(100);
+    lcd.clear();
+    while (Serial.available() > 0) {
+	String temp="";
+ 	char inchar = (char)  Serial.read();
+  	if (inchar != '\r')  temp+=inchar;
+  	lcd.print(temp);   
+    }
+  }
+}
